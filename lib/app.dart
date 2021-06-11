124c2878d1f5a5465bf082b9cdb1fcf6707e64e3
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2021-06-11 11:17:51 +0900

    Update: lib内を整理（フォルダ分け等）

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:memory_share/screens/my_home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
