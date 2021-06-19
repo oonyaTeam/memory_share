2a96d6535aeb0912126413c6c8a78fa6fd8ddaec
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2021-06-19 18:08:42 +0900

    Add: flutter_configの導入、環境変数の設定

---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,1 @@
+-keep class net.takumma.memory_share.BuildConfig { *; }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
+
   runApp(
     ProviderScope(
       child: MyApp(),
