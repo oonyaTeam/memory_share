3e7077390a69f5766157bbc66efc4399ecdafa59
Shoma <sho1228.k@gmail.com>
2021-06-27 14:52:15 +0900

    Update: 既存のボタンをlongButtonコンポーネント使うように修正

--- lib/pages/post_page.dart
+++ lib/pages/post_page.dart
@@ -25,48 +25,9 @@ class _PostPageState extends State<PostPage> {
       ),
       body: Center(
         child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-
           children: [
-            longButton("aiueo",() => log("aaaaaaaaaaaaaaaa")),
-            ButtonTheme(
-              minWidth: 346.0,
-              height: 56.0,
-              child: Container(
-                margin: EdgeInsets.only(top: 522.0),
-                child: RaisedButton(
-                  shape: const StadiumBorder(),
-                  textColor: Colors.white,
-                  child: Text(
-                    "エピソードを追加する",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  onPressed: () {},
-                ),
-              ),
-            ),
-            ButtonTheme(
-              minWidth: 346.0,
-              height: 56.0,
-              child: Container(
-                margin: EdgeInsets.only(top: 11.0),
-                child: RaisedButton(
-                  shape: const StadiumBorder(),
-                  textColor: Colors.white,
-                  child: Text(
-                    "目的地に到着",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  onPressed: () => {print("うんち")},
-                ),
-              ),
-            ),
+            longButton("エピソードを追加する",() => {}),
+            longButton("目的地に到着",() => {})
           ],
         ),
       ),
