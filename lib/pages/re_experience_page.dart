3068751b08074ebb5052b4acb11fa48dff068836
MAAAARCY <masatarou.1003@icloud.com>
2021-07-02 09:18:16 +0900

    Update: move from re_experience_page to episode_view_page

--- lib/pages/re_experience_page.dart
+++ lib/pages/re_experience_page.dart
@@ -1,12 +1,6 @@
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
-import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
-import 'package:flutter/services.dart' show rootBundle;
 
 class ReExperiencePage extends StatelessWidget {
-  ArCoreController arCoreController;
-  Map<int, ArCoreAugmentedImage> augmentedImagesMap = Map();
 
   @override
   Widget build(BuildContext context) {
@@ -14,30 +8,8 @@ class ReExperiencePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("ReExperiencePage"),
       ),
-      body: ArCoreView(
-        onArCoreViewCreated: _onArCoreViewCreated,
-        type: ArCoreViewType.AUGMENTEDIMAGES,
+      body: Container(
       ),
     );
-  }
-
-  void _onArCoreViewCreated(ArCoreController controller) async{
-    arCoreController = controller;
-    //arCoreController.onTrackingImage = _handleOnTrackingImage;
-    //loadImagesDatabase();
-  }
-
-  loadImagesDatabase() async {
-    final ByteData bytes = await rootBundle.load('INSERT IMAGE DATABASE URL');
-    arCoreController.loadAugmentedImagesDatabase(
-        bytes: bytes.buffer.asUint8List());
-  }
-
-  _handleOnTrackingImage(ArCoreAugmentedImage augmentedImage) {
-    if (!augmentedImagesMap.containsKey(augmentedImage.index)) {
-      augmentedImagesMap[augmentedImage.index] = augmentedImage;
-
-      //Navigator.of(context).push(MaterialPageRoute(builder: (context) => EpisodePage()));
-    }
   }
 }
