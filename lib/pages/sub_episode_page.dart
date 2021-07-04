ffdc34b3c3a1c307ac5fdd2a3d399c4a4c41d21d
98zinkun <98zinmeary.0530@gmail.com>
2021-07-04 12:31:39 +0900

    一旦エピソードを追加するボタンを機能させました。 文字はまだ入力できません。

--- lib/pages/sub_episode_page.dart
+++ lib/pages/sub_episode_page.dart
@@ -27,6 +27,15 @@ class _SubEpisodePageState extends State<SubEpisodePage> {
     }
   }
 
+  // List<Color> colorList = [Colors.cyan, Colors.deepOrange, Colors.indigo];
+
+  List<Text> _list = [
+    Text(
+      "初期値",
+      style: TextStyle(fontSize: 20),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,20 +50,97 @@ class _SubEpisodePageState extends State<SubEpisodePage> {
         ),
       ),
       body:Stack(children:[
+
+        Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: _list.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return _list[index];
+                },
+              ),
+            ),
+
+
+
+            // IconButton(
+            //   icon: Icon(Icons.add),
+            //   onPressed: () {
+            //     setState(() {
+            //       _list.add(
+            //         Text(
+            //           "テキスト" + _list.length.toString(),
+            //           style: TextStyle(fontSize: 20),
+            //         ),
+            //       );
+            //     });
+            //   },
+            // ),
+
+          ],
+        ),
+
+        // ListView.builder(
+        //   itemCount: 12,
+        //   itemBuilder: (BuildContext context, int index) {
+        //     return Container(
+        //       height: 80,
+        //       color: colorList[index % colorList.length],
+        //       child: Text('こんにちわ'),
+        //     );
+        //   },
+        // ),
+        // Align(
+        //   alignment: Alignment.bottomCenter,
+        //   child: Container(
+        //     margin: EdgeInsets.only(bottom: 89),
+        //     child:
+        //       longButton("エピソードを追加する",() => {})
+        //   ),
+        // ),
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
-            margin: EdgeInsets.only(bottom: 89),
-            child:
-              longButton("エピソードを追加する",() => {})
+              margin: EdgeInsets.only(bottom: 89),
+              child:
+              longButton("エピソードを追加する",() => {
+                setState(() {
+                  _list.add(
+                    Text(
+                      "テキスト" + _list.length.toString(),
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  );
+                })
+              })
           ),
         ),
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
-            margin: EdgeInsets.only(bottom: 22),
-            child:
-            longButton("目的地に到着",() => onTapArriveButton(context))
+              margin: EdgeInsets.only(bottom: 22),
+              child:
+              longButton("目的地に到着",() => onTapArriveButton(context))
+          ),
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: Container(
+              margin: EdgeInsets.only(right: 22),
+              child:
+              IconButton(
+                icon: Icon(Icons.remove),
+                onPressed: () {
+                  if (_list.length == 1) {
+                    ;
+                  } else {
+                    setState(() {
+                      _list.removeAt(_list.length - 1);
+                    });
+                  }
+                },
+              )
           ),
         ),
       ]),
