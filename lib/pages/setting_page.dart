32394ee06e6e5509494d28bf5536b0eebd26f993
98zinkun <98zinmeary.0530@gmail.com>
2021-07-09 15:45:14 +0900

    セッティングページにおいて「メールアドレスの変更」「パスワードの変更」「ログアウト」イベントを作成した。

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:memory_share/pages/address_page.dart';
+import 'package:memory_share/pages/password_page.dart';
 
 class SettingPage extends StatelessWidget {
   @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -11,11 +14,44 @@ class SettingPage extends StatelessWidget {
       body: Column(
         children: [
           Container(
-              child:IconButton(
-                  icon: Icon(Icons.build_circle),
-                  iconSize: 36,
-                  onPressed: () {}
+              decoration: new BoxDecoration(
+                border: new Border(
+                  bottom: new BorderSide(color: Colors.black),
+                ),
               ),
+              child: ListTile(
+                title: Text('メールアドレスの変更'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => AddressPage()));
+                  }
+              )
+          ),
+          Container(
+              decoration: new BoxDecoration(
+                border: new Border(
+                  bottom: new BorderSide(color: Colors.black),
+                ),
+              ),
+              child: ListTile(
+                  title: Text('パスワードの変更'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => PasswordPage()));
+                  }
+              )
+          ),
+          Container(
+              decoration: new BoxDecoration(
+                border: new Border(
+                  bottom: new BorderSide(color: Colors.black),
+                ),
+              ),
+              child: ListTile(
+                  title: Text('ログアウト'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  onTap:(){}
+              )
           ),
         ],
       ),
