1abe195b80b7fdb85b448c7f9618a5f3290a5a4e
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2021-08-24 22:32:48 +0900

    WiP

--- lib/pages/user_page/user_page.dart
+++ lib/pages/user_page/user_page.dart
@@ -11,60 +11,77 @@ class UserPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final userModel = context.watch<UserModel>();
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("マイページ"),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.build_circle),
-            iconSize: 36,
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const SettingPage()),
+      // appBar: AppBar(
+      //   centerTitle: true,
+      //   title: const Text("マイページ"),
+      //   actions: <Widget>[
+      //     IconButton(
+      //       icon: const Icon(Icons.settings),
+      //       iconSize: 36,
+      //       onPressed: () {
+      //         Navigator.push(
+      //           context,
+      //           MaterialPageRoute(builder: (context) => const SettingPage()),
+      //         );
+      //       },
+      //     ),
+      //   ],
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 200.0,
+            flexibleSpace: const FlexibleSpaceBar(
+              title: Text('これまでの投稿', textScaleFactor: 1),
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.settings),
+                iconSize: 36,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SettingPage()),
+                  );
+                },
+              ),
+            ],
+          ),
+          ListView.separated(
+            separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(height: 16.0);
+            },
+            itemCount: userModel.myMemories.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Column(
+                children: [
+                  Image.network(userModel.myMemories[index].image),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "2021/08/16",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: newTheme().middle,
+                          ),
+                        ),
+                        Text(
+                          "東京都渋谷区",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: newTheme().middle,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               );
             },
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.separated(
-              separatorBuilder: (BuildContext context, int index) {
-                return const SizedBox(height: 16.0);
-              },
-              itemCount: userModel.myMemories.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Column(
-                  children: [
-                    Image.network(userModel.myMemories[index].image),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "2021/08/16",
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              color: newTheme().middle,
-                            ),
-                          ),
-                          Text(
-                            "東京都渋谷区",
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              color: newTheme().middle,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                );
-              },
-            ),
           ),
         ],
       ),
