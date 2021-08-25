56c9b700692b24bee99468eef06b12f9f3bebe7f
Shoma <sho1228.k@gmail.com>
2021-08-25 15:59:07 +0900

    SignUpの方もUI実装

--- lib/pages/sign_up_page/sign_up_page.dart
+++ lib/pages/sign_up_page/sign_up_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:memory_share/models/models.dart';
 import 'package:memory_share/pages/pages.dart';
 import 'package:memory_share/widgets/widgets.dart';
 import 'package:provider/provider.dart';
+import '../../theme.dart';
 import 'sign_up_view_model.dart';
 
 class SignUpPage extends StatelessWidget {
@@ -13,52 +15,112 @@ class SignUpPage extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (_) => SignUpViewModel(AuthRepository()),
       child: Consumer<SignUpViewModel>(
-        builder: (context, signUpViewModel, _) {
-          return Scaffold(
-            appBar: appBarComponent("SignUp"),
-            body: Column(
-              children: [
-                const SizedBox(
-                  height: 24,
-                ),
-                emailPasswordBox(
-                  iconData: Icons.email_outlined,
-                  topText: "Email",
-                  onChanged: signUpViewModel.changeEmail,
-                  width: MediaQuery.of(context).size.width,
-                ),
-                const SizedBox(
-                  height: 4,
-                ),
-                emailPasswordBox(
-                  iconData: Icons.https_outlined,
-                  topText: "Password",
-                  onChanged: signUpViewModel.changePassword,
-                  width: MediaQuery.of(context).size.width
-                ),
-                const SizedBox(
-                  height: 32,
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    await signUpViewModel.signUpWithEmailAndPassword().then((_) {
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (context) => const HomePage()),
-                      );
-                    }).catchError((e) {});
-                  },
-                  child: const Text("SignUp"),
-                ),
-                TextButton(
-                  onPressed: () => Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => const LoginPage()),
+        builder: (context, signUpViewModel, _) => Scaffold(
+          backgroundColor: newTheme().primary,
+          body: Column(
+            children: [
+              // sign upをラップ
+              Container(
+                alignment: Alignment.topLeft,
+                // 480が白いところのheight, 36はSign upのtextのheight
+                margin: EdgeInsets.only(top: (MediaQuery.of(context).size.height - 480 - 36) / 2, left: 24, bottom: (MediaQuery.of(context).size.height - 480 - 36) / 2),
+                child: Text(
+                  "Sign up",
+                  style: GoogleFonts.montserrat(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    height: 1.0
                   ),
-                  child: const Text("Login"),
+                ),
+              ),
+              // そっから下のところ全部をラップ(白いところ)
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.only(topLeft: const Radius.circular(16), topRight: const Radius.circular(16)),
+                ),
+                child: Column(
+                  children: [
+                    // margin取るためにラップ email
+                    Container(
+                      child: emailPasswordBox(
+                        iconData: Icons.email_outlined,
+                        topText: "Email",
+                        onChanged: signUpViewModel.changeEmail,
+                        width: MediaQuery.of(context).size.width
+                      ),
+                      margin: const EdgeInsets.only(top: 24),
+                    ),
+                    // margin取るためにラップ password
+                    Container(
+                      child: emailPasswordBox(
+                        iconData: Icons.https_outlined,
+                        topText: "Password",
+                        onChanged: signUpViewModel.changePassword,
+                        width: MediaQuery.of(context).size.width,
+                      ),
+                      margin: const EdgeInsets.only(top: 16),
+                    ),
+                    // margin取るためにラップ sign inボタン
+                    Container(
+                      child: signInUpButton(
+                        "Sign in",
+                            () async {
+                          await signUpViewModel.signUpWithEmailAndPassword().then((_) {
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(builder: (context) => const HomePage()),
+                            );
+                          }).catchError((e) {});
+                        },
+                        MediaQuery.of(context).size.width
+                      ),
+                      margin: const EdgeInsets.only(top: 32, bottom: 32),
+                    ),
+                    Text(
+                      "または",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: newTheme().deep,
+                        height: 1.0
+                      ),
+                    ),
+                    // 横並びするためにラップ
+                    Container(
+                      margin: const EdgeInsets.only(top: 32, bottom: 16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "アカウントを持っていない？",
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: newTheme().deep,
+                                height: 1.0
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () => Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(builder: (context) => const LoginPage()),
+                            ),
+                            child: Text(
+                              "SignUp",
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: newTheme().primary,
+                                height: 1.0
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
                 ),
               ],
             ),
-          );
-        },
+          )
       ),
     );
   }
