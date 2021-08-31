2cf436d52e6ca85b3994c6d02fdfe8741e20ef80
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2021-08-31 10:02:19 +0900

    Update: mock_web_serverを導入。

--- test/utils/create_memory_test.dart
+++ test/utils/create_memory_test.dart
@@ -1,12 +1,22 @@
+import 'package:flutter_config/flutter_config.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:memory_share/models/models.dart';
+import 'package:memory_share/utils/utils.dart';
+import 'package:mock_web_server/mock_web_server.dart';
 
 void main() {
-  Memory memory;
+  Memory sampleMemory;
+
+  const apiEndpoint = 'http://127.0.0.1:8081/'; // モックAPIのエンドポイント
+  FlutterConfig.loadValueForTesting({'API_ENDPOINT': apiEndpoint});
+
+  final _server = MockWebServer(port: 8081);
 
   setUp(() {
-    memory = Memory(
+    _server.start(); // Mockサーバーを起動
+
+    sampleMemory = Memory(
       memory: "this is sample main episode",
       latLng: const LatLng(34.8532, 136.5822),
       seenAuthor: ["author1"],
@@ -23,9 +33,13 @@ void main() {
     );
   });
 
-  group("createMemory", () {
+  // 全てのテストが終了したら、Mockサーバーを閉じる。
+  tearDown(_server.shutdown);
+
+  group("createMemory testing", () {
     test("test", () async {
-      // await createMemory(memory);
+      _server.enqueue(httpCode: 201, body: '{ "msg: "OK" }');
+      await createMemory(sampleMemory);
       expect("test", "test");
     });
   });
---
+++ test/utils/fetch_memories_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  setUp(() {});
+
+  group("Fetch Memories test", () {
+    test("test", () async {
+      // final List<Memory> memories = await fetchMemories();
+      expect("test", "test");
+    });
+  });
+}
