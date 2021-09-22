8b94a2ad53bff5df3a2845614312542d0adf5c3c
MAAAARCY <masatarou.1003@icloud.com>
2021-09-22 14:30:25 +0900

    Fix: cupertino.dartを削除

--- lib/pages/sub_episode_page/sub_episode_page.dart
+++ lib/pages/sub_episode_page/sub_episode_page.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:image_picker/image_picker.dart';
