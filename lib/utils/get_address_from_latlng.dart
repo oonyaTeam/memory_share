b12933c62be9c013793319a396a9d2bdc576097e
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2021-09-29 23:31:03 +0900

    WiP

--- lib/utils/get_address_from_latlng.dart
+++ lib/utils/get_address_from_latlng.dart
@@ -1,3 +1,13 @@
-String getAddressFromLatLng() {
-  return '';
+import 'package:geocoding/geocoding.dart';
+
+Future<String> getAddressFromLatLng({
+  required double latitude,
+  required double longitude,
+}) async {
+  final List<Placemark> placemarkList = await placemarkFromCoordinates(
+    latitude,
+    longitude,
+    localeIdentifier: 'ja',
+  );
+  return placemarkList.map((placemark) => placemark.name ?? "").join(' ');
 }
